35acfd61cc8f163bda7a75fe9da578f9245fd8a5
Owen Davies <owen@odavi.es>
2020-12-19 07:51:08 +0000

    Add day 19

---
+++ bin/day19-2.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $rules = {};
+while(<>) {
+    chomp;
+
+    last if !$_;
+
+    my ($num, $rule) = split ': ', $_;
+    $rules->{$num} = $rule;
+}
+
+my @messages;
+while(<>) {
+    chomp;
+
+    push @messages, $_;
+}
+
+my $re = build_regex(0);
+print scalar grep { $_ =~ /^$re$/ } @messages;
+print "\n";
+
+# XXX: only works with modified input for rule 11
+sub build_regex {
+    my $num = shift;
+
+    my $rule = $rules->{$num};
+
+    if ($num == 8) {
+        return build_regex(42) . '+';
+    }
+
+    if (my ($ident) = $rule =~ /"(\w)"/) {
+        return $ident;
+    } 
+
+    my @ors = split ' ', $rule;
+    my $or_regex = '';
+    my $bracket = 0;
+    for my $o (@ors) {
+        if ($o =~ /^\d+/) {
+            $or_regex .= build_regex($o);
+        }
+        else {
+            $or_regex .= $o;
+            $bracket = 1;
+        }
+    }
+
+    $or_regex = "($or_regex)" if $bracket;
+
+    return $or_regex;
+}
+
